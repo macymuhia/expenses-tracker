c7756b15ad28dce2c79ba593148549f80614bcd9
macymuhia <macymuhia@gmail.com>
2020-08-16 21:27:55 +0300

    Setup Vue App modules

--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'bootstrap-css-only/css/bootstrap.min.css'
+import 'mdbvue/lib/css/mdb.min.css'
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+import router from './router/index'
+import store from './store/index'
+import * as mdbvue from 'mdbvue'
+import { ValidationObserver, ValidationProvider, extend } from "vee-validate"
+import * as rules from "vee-validate/dist/rules"
+import { messages } from "vee-validate/dist/locale/en.json";
+
+for (const component in mdbvue) {
+Vue.component(component, mdbvue[component])
+}
+Object.keys(rules).forEach(rule => {
+  extend(rule, {
+    ...rules[rule],
+    message: messages[rule]
+  });
+});
+Vue.component("ValidationObserver", ValidationObserver);
+Vue.component("ValidationProvider", ValidationProvider);
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
